86fa6253eef8b767b4f45f63a971da0ad0dd7974
Finn Buick <finnbuick@gmail.com>
2020-12-03 07:20:33 +1100

    day 2 done

--- Day2/sol.py
+++ Day2/sol.py
@@ -3,21 +3,21 @@ import re
 with open("input.txt") as f:
     lines = f.readlines()
 
-count1 = 0
-count2 = 0
+    count1 = 0
+    count2 = 0
 
-for line in lines:
-    match = re.findall(r'(\d+)-(\d+) ([a-z]): (\w*)',line)[0]
-    lower = int(match[0])
-    upper = int(match[1])
-    letter = match[2]
-    password = match[3]
+    for line in lines:
+        match = re.findall(r'(\d+)-(\d+) ([a-z]): (\w*)',line)[0]
+        lower = int(match[0])
+        upper = int(match[1])
+        letter = match[2]
+        password = match[3]
 
-    if lower <= password.count(letter) <= upper:
-        count1 += 1
+        if lower <= password.count(letter) <= upper:
+            count1 += 1
 
-    if (password[lower-1] == letter) ^ (password[upper-1] == letter):
-        count2 += 1
+        if (password[lower-1] == letter) ^ (password[upper-1] == letter):
+            count2 += 1
 
-print("Part 1", count1)
-print("Part 2", count2)
+    print("Part 1", count1)
+    print("Part 2", count2)
