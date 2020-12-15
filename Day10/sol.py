656bfc1f7a3690cbee9b36461f4373a0d0f5e0bd
FinnBuick <finnbuick@gmail.com>
2020-12-15 20:42:58 +1100

    day 10 part 2

--- Day10/sol.py
+++ Day10/sol.py
@@ -3,19 +3,35 @@ from functools import reduce
 with open("Day10/input.txt", "r") as f:
     lines = f.readlines()
     nums = list(map(int, lines))
-
-
-def findDiffs(nums):
+    nums.append(0)
     nums = sorted(nums)
-    nums.insert(0, 0)
-    last = nums[-1] + 3
-    nums.append(last)
+    lastAdapter = nums[-1] + 3
+    nums.append(lastAdapter)
 
+
+def part_1(nums):
     diffs = [0] * len(nums)
     for i in range(1, len(nums)):
-        diff = nums[i] - nums[i-1]
-        diffs[i] = diff
-    return [diffs.count(1), diffs.count(3)]
+        diffs[i] = nums[i] - nums[i-1]
+    print(diffs.count(1) * diffs.count(3))
+    return diffs
 
 
-print(reduce(lambda x, y: x*y, findDiffs(nums)))
+DP = {}
+
+
+def dp(i):
+    if i == len(nums)-1:
+        return 1
+    if i in DP:
+        return DP[i]
+    ans = 0
+    for j in range(i+1, len(nums)):
+        if nums[j] - nums[i] <= 3:
+            ans += dp(j)
+    DP[i] = ans
+    return ans
+
+
+# print(reduce(lambda x, y: x*y, part_1(nums)))
+print(dp(0))
